9687450fa4d8cfb91b4864cd7a608d65435ed1b7
KidMix945 <164611159+KidMix945@users.noreply.github.com>
2024-03-23 10:39:32 +0300

    Add files via upload

---
+++ ModelingClasses/TestTask/Money.cs
@@ -0,0 +1,16 @@
+﻿using static TestTask.Enumeration;
+
+namespace TestTask
+{
+    internal class Money
+    {
+        public decimal Amount { get; set; }
+        public Currency Currency { get; set; }
+
+        public Money(decimal amount, Currency currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+    }
+}
---
+++ ModelingClasses/TestTask/Person.cs
@@ -0,0 +1,73 @@
+﻿using static TestTask.Enumeration;
+
+namespace TestTask
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public List<Money> MoneyList { get; set; }
+
+        public Person(string name, int age, Gender gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            MoneyList = new List<Money>();
+        }
+
+        public void AddMoney(Money money)
+        {
+            Money existingMoney = MoneyList.FirstOrDefault(m => m.Currency == money.Currency);
+            if (existingMoney != null)
+            {
+                existingMoney.Amount += money.Amount;
+            }
+            else
+            {
+                MoneyList.Add(money);
+            }
+        }
+
+        public void SpendMoney(Money money)
+        {
+            foreach (var m in MoneyList)
+            {
+                if (m.Currency == money.Currency)
+                {
+                    if (m.Amount >= money.Amount)
+                    {
+                        m.Amount -= money.Amount;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недостаточно средств");
+                    }
+                }
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nAge: {Age}\nGender: {Gender}\nMoney:");
+
+            foreach (var money in MoneyList)
+            {
+                if (money.Currency == Currency.RUB)
+                {
+                    Console.WriteLine($"{money.Amount} RUB");
+                }
+                if (money.Currency == Currency.USD)
+                {
+                    Console.WriteLine($"{money.Amount} USD");
+                }
+                if (money.Currency == Currency.EUR)
+                {
+                    Console.WriteLine($"{money.Amount} EUR");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
