e07eb428817ae12465ce7beee5c0209d0648088d
KidMix945 <164611159+KidMix945@users.noreply.github.com>
2024-03-23 10:35:58 +0300

    Add files via upload

---
+++ ModelingClasses/TestTask/Enumeration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    internal static class Enumeration
+    {
+            public enum Gender
+            {
+                Male,
+                Female
+            }
+
+            public enum Currency
+            {
+                USD,
+                EUR,
+                RUB
+            }
+
+    }
+}
